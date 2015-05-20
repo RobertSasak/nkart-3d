797f833421fdaf8e83e0c602f011217ed808b862
Robert Sasak <robert.sasak@gmail.com>
2015-05-20 15:42:22 +0200

    Speed up.

--- main.js
+++ main.js
@@ -2,29 +2,7 @@
 
 (function (Cesium) {
 
-	var viewer = new Cesium.Viewer('cesiumContainer', {
-		//baseLayerPicker: true
-		animation: false,
-		timeline: false,
-	});
-
-	var scene = viewer.scene;
-
-	function initTerrain() {
-		var cesiumTerrainProviderMeshes = new Cesium.CesiumTerrainProvider({
-			url: '//cesiumjs.org/stk-terrain/tilesets/world/tiles',
-			credit: ''
-		});
-
-		scene.terrainProvider = cesiumTerrainProviderMeshes;
-
-		viewer.scene.camera.flyTo({
-			destination: Cesium.Cartesian3.fromDegrees(5.338519, 60.394673, 1000000.0),
-			duration: 15
-		});
-	}
-
-	function initLayer() {
+	function getProviderForNorgeskart() {
 		var provider = new Cesium.TileMapServiceImageryProvider({
 			url: 'http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=norges_grunnkart&zoom={z}&x={x}&y={y}#',
 			maximumLevel: 18,
@@ -37,15 +15,35 @@
 				.replace('{x}', x)
 				.replace('{y}', y)
 				.replace('{z}', level);
-			console.log(url);
 
 			return Cesium.ImageryProvider.loadImage(this, url);
 		};
-
-		viewer.scene.imageryLayers.addImageryProvider(provider);
+		return provider;
 	}
 
+	function initTerrain() {
+		var cesiumTerrainProviderMeshes = new Cesium.CesiumTerrainProvider({
+			url: '//cesiumjs.org/stk-terrain/tilesets/world/tiles',
+			//url: '//assets.agi.com/stk-terrain/world',
+			credit: '',
+			requestWaterMask: true,
+		});
+		scene.terrainProvider = cesiumTerrainProviderMeshes;
+	}
+
+	// Home position
+	Cesium.Camera.DEFAULT_VIEW_RECTANGLE = Cesium.Rectangle.fromDegrees(5, 60, 8, 63);
+
+	var viewer = new Cesium.Viewer('cesiumContainer', {
+		imageryProvider: getProviderForNorgeskart(),
+		baseLayerPicker: false,
+		animation: false,
+		timeline: false,
+		sceneModePicker: false,
+	});
+
+	var scene = viewer.scene;
+
 	initTerrain();
-	initLayer();
 
 })(Cesium);
